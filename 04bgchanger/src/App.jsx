63e496c53a76774bafc2c442cb6d7beb915d1e52
Ahsmmed Sidan <sidan4321@gmail.com>
2023-11-17 18:42:40 +0530

    completed bgcolor project

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -1,11 +1,32 @@
 import { useState } from "react";
-import { useEffect } from "react";
 function App() {
-  const initialColor = localStorage.getItem("selectedColor") || "olive";
+  let initialval;
 
-  const [color, setColor] = useState(initialColor);
+  // when we are using the local storage if we are storing
+  // string , there is no need to use stringify and parse
+  // but in this case we just used if for the purpose of study
+  // Here first we took one item from the localstorage
+  // and named it initialColor
+  // the value of it will be null , so we change it into olive
+  // then
+  // whenever we press the button we add that value to localstorage
+  //when we refresh the webpage we will check whether the value of
+  //initialColor is null or not
+  //id it is not null we will parse it and store it into initialval
 
-  localStorage.setItem("selectedColor", color);
+  const initialColor = localStorage.getItem("selectedColor");
+  if (initialColor === null) {
+    initialval = "olive";
+    localStorage.setItem("initialColor", JSON.stringify(initialval));
+  } else {
+    initialval = JSON.parse(initialColor);
+  }
+  const [color, setColor] = useState(initialval);
+
+  const handleColorChange = (newColor) => {
+    setColor(newColor);
+    localStorage.setItem("selectedColor", JSON.stringify(newColor));
+  };
 
   return (
     <div
@@ -21,14 +42,14 @@ function App() {
         gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl"
         >
           <button
-            onClick={() => setColor("red")}
+            onClick={() => handleColorChange("red")}
             className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
             style={{ backgroundColor: "red" }}
           >
             Red
           </button>
           <button
-            onClick={() => setColor("green")}
+            onClick={() => handleColorChange("green")}
             className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
             style={{ backgroundColor: "Green" }}
           >
@@ -36,7 +57,7 @@ function App() {
           </button>
 
           <button
-            onClick={() => setColor("blue")}
+            onClick={() => handleColorChange("blue")}
             className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
             style={{ backgroundColor: "blue" }}
           >
