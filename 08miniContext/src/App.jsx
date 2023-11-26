87ba3efc886f48ceb8bf6d71269d17929e577dad
Ahsmmed Sidan <sidan4321@gmail.com>
2023-11-27 01:24:52 +0530

    completed some mini task on context

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
+import Login from "./Components/Login";
+import Profile from "./Components/Profile";
+
 import UserContextProvider from "./Context/UserContextProvider";
 
 function App() {
   return (
     <UserContextProvider>
       <h1>React with Zidane</h1>
+      <Login />
+      <Profile />
     </UserContextProvider>
   );
 }
